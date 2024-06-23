c1db39b1a130f962f30d365f9af039fec89a3b3a
mrNitesh14 <nitesh.rathod@spit.ac.in>
2024-06-23 19:44:41 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -2,14 +2,10 @@ require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 
-// const timings = require("./routes/timings");
-// const users = require("./routes/users");
-// const auth = require("./routes/auth");
 const PORT = process.env.PORT || 3000;
 const { connectDB } = require("./db/db");
-// app.use(bodyParser.json());
+
 app.use(express.json());
 
 const bodyparser = require("body-parser");
@@ -19,9 +15,6 @@ app.use(cors());
 
 connectDB();
 
-// app.use("/api/v1/timings/", timings);
-// app.use("/api/v1/users/", users);
-// app.use("/api/v1/auth/", auth);
 
 app.get("/", (req, res) => {
   res.send(JSON.stringify({ message: "Hello World" }));
